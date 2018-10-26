fdc906f50e58326a16a5e04f150566cecacf0cd2
Kavindu Chinthana <it14136252@my.sliit.lk>
2018-10-27 01:05:27 +0530

    Html for result page added

---
+++ result.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>CSRF-Protection Demo Using Double Submit Cookies Pattern</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+<script>
+</script>
+
+<body>
+
+	<div class="jumbotron text-center">
+		<h4>CSRF-Protection Demo Using Double Submit Cookies Pattern</h4>
+	</div>
+
+	<div class="container">
+		<div class="row">
+
+		</div>
+	</div>
+
+</body>
+
+</html>
