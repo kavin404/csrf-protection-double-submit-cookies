1fe2bf2042314eee0a30cb22335cedc51af7249f
Kavindu Chinthana <it14136252@my.sliit.lk>
2018-10-27 08:31:11 +0530

    Code refactoring

--- demo_action.php
+++ demo_action.php
@@ -1,21 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-	<title>CSRF-Protection Demo Using Double Submit Cookies Pattern</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
+<?php include 'head.php'; ?>
 
 <body>
 
-	<div class="jumbotron text-center">
-		<h4>CSRF-Protection Demo Using Double Submit Cookies Pattern</h4>
-	</div>
-
+ 	<?php include 'common.php'; ?>
 	<div class="container">
 		<div class="row">
 
