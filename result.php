c59c5b4848f8ece3b8151d51d49d75fec8f7727d
Kavindu Chinthana <it14136252@my.sliit.lk>
2018-10-27 09:58:33 +0530

    Comments added

--- result.php
+++ result.php
@@ -2,6 +2,12 @@
     if (isset($_POST['resource_name']) && isset($_POST['csrfToken'])) {
         $resource = $_POST['resource_name'];
         $valid_request = false;
+
+        /***
+         * The following code checks if the request csrfToken is 
+         * identical to the csrf token contained in the cookie
+         * If they are equal action would be allowed
+         */
         if(isset($_COOKIE['tokenID'])){
             if($_COOKIE['tokenID'] == $_POST['csrfToken']){
                 $valid_request = true;
